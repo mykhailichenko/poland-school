ef7332444fa605eddf970603a2251740abda26a9
i.mykhailichenko <i.mykhailichenko@abtollc.com>
2022-12-30 04:56:58 +0200

    send email

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-    </Routes>
+    <HomePage />
   );
 }
 
